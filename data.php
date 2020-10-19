8559366bc120e24b6b1895793b39a3abb2f6c7c3
Aseel Mohamad <aseel3sal@gmail.com>
2020-10-19 14:25:27 +0200

    add prettier file

--- data.php
+++ data.php
@@ -1,4 +1,5 @@
 <?php 
+declare(strict_types=1);
 
 $articles = [
     [ 'title'=> 'Förskollärarna slår larm: Vi hinner inte med',
--- index.php
+++ index.php
@@ -14,8 +14,104 @@ $title = 'Fake News!';
 </head>
 <body>
     <div class ="container">
-        <h1> The News Feed </h1> 
-        <div class= "article 1">
+        <h1> The News Feed </h1>
+        <section>
+            <div class= "article1">
+            <img src="" alt="">
+            <header>
+                 <h1> <a href="">     </a>  </h1>
+                 <a href=""> Read here >>> </a>
+                 
+            </header>
+            <footer>
+                <!-- Author's name -->
+                <p> </p> 
+                 <!-- date -->
+                <p> </p>
+
+            </footer>
+
+
+
+             </div>
+
+        <div class= "article1">
+            <img src="" alt="">
+            <header>
+                 <h1> <a href="">     </a>  </h1>
+                 <a href=""> Read here >>> </a>
+                 
+            </header>
+            <footer>
+                <!-- Author's name -->
+                <p> </p> 
+                 <!-- date -->
+                <p> </p>
+
+            </footer>
+
+
+
+        </div>
+
+        <div class= "article1">
+            <img src="" alt="">
+            <header>
+                 <h1> <a href="">     </a>  </h1>
+                 <a href=""> Read here >>> </a>
+                 
+            </header>
+            <footer>
+                <!-- Author's name -->
+                <p> </p> 
+                 <!-- date -->
+                <p> </p>
+
+            </footer>
+
+
+
+        </div>
+
+        <div class= "article1">
+            <img src="" alt="">
+            <header>
+                 <h1> <a href="">     </a>  </h1>
+                 <a href=""> Read here >>> </a>
+                 
+            </header>
+            <footer>
+                <!-- Author's name -->
+                <p> </p> 
+                 <!-- date -->
+                <p> </p>
+
+            </footer>
+
+
+
+        </div>
+
+        <div class= "article1">
+            <img src="" alt="">
+            <header>
+                 <h1> <a href="">     </a>  </h1>
+                 <a href=""> Read here >>> </a>
+                 
+            </header>
+            <footer>
+                <!-- Author's name -->
+                <p> </p> 
+                 <!-- date -->
+                <p> </p>
+
+            </footer>
+
+
+
+        </div>
+
+        <div class= "article1">
             <img src="" alt="">
             <header>
                  <h1> <a href="">     </a>  </h1>
@@ -46,6 +142,11 @@ $title = 'Fake News!';
 
 
 
+
+
+
+
+         </section>
 
     </div>
 
