086135c0cb4fb5309000bf7d3a3603e14b3be8b3
Aseel Mohamad <aseel3sal@gmail.com>
2020-10-19 12:57:59 +0200

    add arrays to data file

---
+++ data.php
@@ -0,0 +1,162 @@
+<?php 
+
+$articles = [
+    [ 'title'=> 'Förskollärarna slår larm: Vi hinner inte med',
+      'Author'=> 'Malin Wigen',
+      'content'=> 'Hälften av förskollärarna hinner inte med sitt jobb.
+
+      Det visar en ny rapport om deras arbetssituation.
+      
+      – Det är djupt oroväckande, säger Johanna Jaara Åstrand, Lärarförbundets ordförande.
+      
+      I en ny rapport från Lärarförbundet slår förskollärarna larm om sin arbetssituation. En majoritet av de tillfrågade lärarna jobbar med för stora barngrupper – det vill säga att de är större än vad Skolverket rekommenderar. Dessutom uppger fem av tio lärare att de inte hinner med sitt jobb.
+      
+      ”Barngruppen består i dag av 14 barn i åldrarna ett till tre år, som alla har vistelsetid cirka 07.30-17.00. Vi täcker inte riktigt upp på eftermiddagarna då vi är två pedagoger efter klockan 13.30. På eftermiddagen blir det rätt körigt, barnen ska väckas, tas upp från sina vagnar, bytas blöjor och förbereda inför mellanmål…Ofta är man själv sedan från 16.00 med cirka tolv småbarn”, lyder ett av vittnesmålen.
+      
+      ”De är på tok för få”
+      Enligt Johanna Jaara Åstrand, ordförande för Lärarförbundet, visar rapporten en annan verklighet om förskollärarnas situation än den som Skolverket och regeringen förmedlat tidigare i år – att barngrupperna inte varit så små sedan 1992. Förbundets undersökning visar att bara 262 av 1 306 svarande förskollärare arbetar i en barngrupp där riktlinjen följs. Och det var i oktober förra året, i februari ska förutsättningarna har förvärrats.
+      
+      – Det allvarliga är att den statistik som Skolverket samlar in årligen bara ger en liten bild av sanningen. Förskolan har inte känt igen sig i vad som rapporterats gällande storleken på barngrupperna. När statsministern har lutat sig tillbaka och sagt ”jajamen, det går åt rätt håll” så har vi stått där med uppspärrade ögon och undrar var han får det ifrån?
+      
+      – Det som också är allvarligt är att hälften uppger att de inte hinner med sitt uppdrag. Det är djupt oroväckande. Det är tok för få förskollärare och för stora barngrupper. Det rimmar illa i och med att skollagen det senaste året stärkts. Det spelar ingen roll vad man skriver i styrdokumenten när man inte ger förskollärarna de bästa förutsättningarna att utföra sitt jobb.',
+      'like counter'=>'100',
+      'Published date'=> 'TIS 22 SEP 2020',
+      'img'=>'https://imgs.aftonbladet-cdn.se/v2/images/51be2555-1097-4120-864e-5aca619c9141?fit=crop&h=601&q=50&w=900&s=38111d2c8dd03606a3995d941a26dfd090ca3274',
+      //'comments'=> '',
+      'resource'=> 'Aftonbladet',
+    ], 
+
+    [ 'title'=> 'Victor, 4, är sjuk i PKU', 
+      'Author'=> 'Malin Wigen',
+      'content'=> 'För fyraårige Victor är dieten A och O.
+
+      Om det slarvas med den kan han få bestående och allvarliga hjärnskador.
+      
+      – Man har inte det vanliga föräldraansvaret. Sköter vi oss så får han inga hjärnskador, sköter vi oss inte så kan han drabbas av utvecklingsstörning, säger pappa Marcus.
+      Att ta en extra pannkaka, eller en extra macka, kan låta som en bagatell för gemene man men för fyraårige Victor kan det leda till allvarliga hjärnskador och utvecklingsstörning. Han lider av den sällsynta sjukdomen PKU som påverkar ämnesomsättningen.
+
+–     Det är en allvarlig sjukdom som inte syns, säger pappan Marcus Strandepil.',
+      'like counter'=> '100',
+      'Published date'=> 'LÖR 18 OCT 2020',
+      'img'=> 'https://imgs.aftonbladet-cdn.se/v2/images/87af8722-fe11-40de-af15-827418d65888?fit=crop&h=674&q=50&w=900&s=5f082367b622bd1f46db692d3ff304980dd26dbb',
+      //'comments'=> '',
+      'resource'=> 'Aftonbladet',
+    ], 
+
+    [ 'title'=> 'The golden hour: 15 ways to change your life in a work-from-home lunch break',
+      'Author'=> 'Elle Hunt',
+      'content'=> 'Phone a friend – or make a new one
+      Being separated from colleagues has stripped us of a lot of routine human interaction. Your lunch hour is often a convenient time to reconnect with old friends, says Clare Evans, a time management coach. “Often, we’re too busy to make the time in the evening, but a quick catch-up can get you back in touch and talking,” she says. Otherwise, you could challenge yourself to speak to a stranger while you are in a shop or a cafe. “Just have a chat as you grab your lunch, even if it’s the classic British opening gambit of discussing the weather,” says Evans.
+      
+      Visit your favourite tree
+      Don’t pretend you don’t have one. The key is to go outside, away from your computer screen, and if you combine it with exercise, so much the better. Susan Saunders, the author of The Age-Well Plan, says: “Seeing a tree combines so many of the habits that contribute to longevity: being outdoors in daylight, vital to keep our circadian clocks ticking accurately; exercise from walking; a chance to savour the moment. And even one lonely tree provides us with a little green space.”',
+      'like counter'=> '225',
+      'Published date'=> 'Thu 24 Sep 2020',
+      'img'=> 'https://i.guim.co.uk/img/media/a49b0b80c650c06a067a70c81fad5cdee7da24d6/0_120_5600_3362/master/5600.jpg?width=1020&quality=85&auto=format&fit=max&s=03da0b4c5c915d2656f0dc419d9de609',
+     // 'comments'=> '',
+      'resource'=> 'The Guardian',
+    ],
+
+    [ 'title'=> 'I lent my mother my old phone. Now she’s read my text messages – and discovered untold secrets',
+      'Author'=> 'Zoe Williams',
+      'content'=> 'If you could call anywhere the canary in the coalmine of incompetence and chaos, it would be care homes. At the start of the coronavirus crisis, it was the UK health secretary, Matt Hancock, and his “protective ring”, which, in reality, meant insufficient testing, inadequate PPE and mass do-not-resuscitate orders because, come on, if you don’t try to save people’s lives, who can truthfully say you failed? By mid-June, care homes were the high-water mark of the tragedy, with more than 16,000 dead in that one setting. Recently, the appalling cost of mismanagement on a human level has been apparent in these homes: patients with dementia losing the will to live without family visits, a government unable to muster a response to Covid and also blinded by it, apparently devoid of feeling for anyone with anything other than coronavirus.
+
+      In the midst of it all, my mother ended up in a care home, following an event that we would normally call falling over but, for reasons of endemic ageism (in my view), we now call “having a fall”. The two days she spent in hospital were worse in terms of visiting, since not only could my sister and I not see her, but we could feel the hot anxiety of the nursing staff as we hovered outside the door trying to pass her a power bank for her phone.',
+      'like counter'=> '75',
+      'Published date'=> 'Sun 18 Oct 2020',
+      'img'=> 'https://i.guim.co.uk/img/media/9553aee8b1b04c50e21a589115e2adc46109d337/1034_343_4693_2816/master/4693.jpg?width=620&quality=85&auto=format&fit=max&s=36075f14aaff3bc670af0add0289066d',
+      //'comments'=> '',
+      'resource'=> 'The Guardian',
+    ], 
+
+    [ 'title'=> '',
+      'Author'=> '',
+      'content'=> '',
+      'like counter'=> '',
+      'Published date'=> '',
+      'img'=> '',
+      'comments'=> '',
+      'resource'=> '',
+    ],
+  
+]
+
+
+
+
+$authors =[
+    [
+        'full name'=> 'Malin Wigen',
+        'id'=> 'Malin Wigen är reporter och redaktör för Aftonbladet Family sedan mars 2019. På Family får du svar på frågor som rör familjelivet och ta del av tabubelagda ämnen samt livsöden som berör.
+                Malin började på Aftonbladet 2016 och var då en del av tidningens sociala medier-redaktion. Innan dess var hon på Nyheter24 i cirka två års tid, både som praktikant och anställd.
+                Utbildning: Malin Wigen har gått ”Journalistik för akademiker” på Södertörns högskola. Tidigare har hon studerat tre år på Språkkonsultprogrammet på Umeå universitet och läst kurser inom statsvetenskap och nationalekonomi på Stockholms universitet.',
+
+    ],
+    [
+        'full name'=> 'Elle Hunt',
+        'id'=> 'Elle Hunt is a freelance journalist',
+
+    ],
+    [
+        'full name'=> 'Zoe Williams',
+        'id'=> 'Zoe Williams is a Guardian columnist. ',
+
+    ],
+    [
+        'full name'=> '',
+        'id'=> '',
+
+    ],
+    [
+        'full name'=> '',
+        'id'=> '',
+
+    ],
+]
+    
+
+
+
+
+
+
+
+
+
+
+
+]
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
