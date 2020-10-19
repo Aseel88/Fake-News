b8579075e6a0d39e1f440f0fa08ec1956a30d24f
Aseel Mohamad <aseel3sal@gmail.com>
2020-10-19 13:29:26 +0200

    add to index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+$title = 'Fake News!';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <div class ="container">
+        <h1> The News Feed </h1> 
+        <div class= "article 1">
+            <img src="" alt="">
+            <header>
+                 <h1> <a href="">     </a>  </h1>
+                 <a href=""> Read here >>> </a>
+                 
+            </header>
+            <footer>
+                <!-- Author's name -->
+                <p> </p> 
+                 <!-- date -->
+                <p> </p>
+
+            </footer>
+
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+</body> 
+</html>
+
