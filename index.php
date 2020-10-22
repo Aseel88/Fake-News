eabc0eda70de4ee8ae5593d51f3c9ae4bfd56b5f
Aseel Mohamad <aseel3sal@gmail.com>
2020-10-23 00:35:00 +0200

    changing index.php

--- index.php
+++ index.php
@@ -9,65 +9,68 @@ declare(strict_types=1);?>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Fake News!</title>
-    <link rel="stylesheet" href="style.css" />
-    <link href="https://fonts.googleapis.com/css2?family=Vast+Shadow&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="mobile.css" />
+    <!--<link href="https://fonts.googleapis.com/css2?family=Vast+Shadow&display=swap" rel="stylesheet">-->
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 </head>
 <body>
     <div class="list">
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTs0WbR1a0ETshuDqauez59x6W5Y7BykBI5vA&usqp=CAU"> 
-    <nav>
-        <div class="links"><a href="">Services</a></div>
-        <div class="links"><a href="">About</a></div>
-        <div class="links"><a href="">Work</a></div>
-        <div class="links"><a href="">Contact</a></div>
-    </nav>
+        <img src=""> 
+    
+        <nav>
+           <a href ="#"> International </a>
+           <a href ="#"> Local </a>
+           <a href ="#"> Business </a>
+           <a href ="#"> Science </a>
+
+        </nav>
     </div>
-      
-    <h1> The News Feed </h1>
-    <?php foreach ($articles as $article):?> 
-    <div class ="container">        
-            <main>
-                      
-        <header>  
-                   <!--<div class= "article"> -->               
-                
-                   <img src="<?php echo getImg($article); ?>" >                           
-         </header> 
-         <section>  
-             <div class ="description"> 
-            <div class="like" >
-                <i class="far fa-thumbs-up"></i>
-                 <?php echo getLikeCounter($article); ?>
-           </div>
-                 <h1 ><a href="#"> <?php echo getTitle($article) ; ?> </a> </h1>
-                 
-                 <a href="article.php?id=<?php echo $article['id']; ?>">Read here </a>
-                
+    <main>
+        <h1> News Feed </h1>
+        <section>            
+            <div class ="various_news">
+                <a href =""> Coronavirus pandemic</a>
+                <a href =""> iPhone 12 review: Upgrade for the camera, not 5G</a>
+                <a href =""> Teabags: Which brands contain plastic?</a>
+                <a href =""> Norway funds satellite map of world's tropical forests</a>
             </div>
-        </section> 
-                   
-        
-        <footer>
+
+            <div class ="articles">  
+                <?php foreach ($articles as $article):?>           
+                <header>                             
+                   <img src="<?php echo getImg($article); ?>" >                           
+               </header> 
+               <div class ="description"> 
+                  <div class="like" >
+                    <i class="far fa-thumbs-up"></i>
+                   <?php echo getLikeCounter($article); ?>
+                  </div>
+                 <h1 ><a href="#"> <?php echo getTitle($article) ; ?> </a> </h1>                 
+                 <a class ="read" href="article.php?id=<?php echo $article['id']; ?>">Read here </a>
+                
+                 </div>
+              
                
-               <div class="author"> <p> Written by: <?php echo getAuthor( $article); ?> </p> </div>
-                
-               <div class="date"> <p><?php echo getPublishedDate($article); ?> </p> </div>
-                
-                          
+              <footer>               
+               <div class="author"> <p> Written by: <?php echo getAuthor( $article); ?> </p> </div>                
+               <div class="date"> <p><?php echo getPublishedDate($article); ?> </p> </div>          
+              </footer>           
+              <?php endforeach; ?>
+                <br>       
             
+           </div>  
 
-        </footer>           
-                        
-            <?php endforeach; ?>
-            <br>
+            <div class ="authors">
+                <h3> About authors! </h3>
+                <?php foreach ($articles as $article):?>  
+                <div class="about_author">                     
+                    <a  href= "authors.php?id=<?php echo $article['author']; ?>"><?php echo getAuthor( $article); ?>  </a> 
+                </div>
+                <?php endforeach; ?>
+                <br>  
+                </div> 
+            </div>
+        </section>    
     </main>
-    </div>
-           
-        
-
-   
-
-</body> 
+</body>
 </html>
-
